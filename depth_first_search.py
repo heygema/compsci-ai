dea82046c7beddcce2a015a6a87cdd10d5f36449
Gema Anggada <gem.anggada@gmail.com>
2017-09-29 05:26:44 +0700

    Create depth_first_search.py

---
+++ depth_first_search.py
@@ -0,0 +1,31 @@
+graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B']),
+         'F': set(['C'])}
+
+
+def dfs(graph, start):
+    visited, stack = set(), [start]
+    # selama ada node di stack melakukan search
+    while stack:
+        print("kondisi stack skrg {}".format(stack))
+        node = stack.pop()
+        print("current node  {}".format(node))
+        print("stack setelah node divisit {}".format(stack))
+        print("visited node {}\n---------".format(visited))
+        if node not in visited:
+            visited.add(node)
+            print("visited added with {}\n---------".format(visited))
+            # masukan kedalam stack graph yang tersambung ke node
+            stack.extend(graph[node] - visited)
+    return visited
+
+def iddfs(graph, start):
+    return 0
+
+if __name__ == "__main__":
+    print("dfs solution")
+    vs = dfs(graph, 'A')
+    print("visited graph: {}".format(vs))
